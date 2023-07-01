c6d1db2faa1fc154377b825a9503af8270108ce7
Roman Tentser <rtentser@yandex.ru>
2023-07-01 14:04:12 +0700

    Implement

---
+++ index.js
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import yargs from "yargs/yargs";
+import { hideBin } from "yargs/helpers";
+import http from "http";
+import url from "node:url";
+import fs from "fs/promises";
+
+const config = JSON.parse(await fs.readFile("./config.json"), {
+  encoding: "utf8",
+});
+
+yargs(hideBin(process.argv))
+  .command(
+    "current",
+    "Show current weather",
+    (yargs) => {
+      return yargs.option("city", {
+        alias: "c",
+        demandOption: true,
+        type: "string",
+      });
+    },
+    (argv) => {
+      const baseURL = "http://api.weatherstack.com/current";
+
+      const params = new URLSearchParams();
+      params.append("access_key", process.env.API_KEY);
+      params.append("query", argv.city);
+      // params.append("units", config.units);
+      // params.append("language", config.language);
+
+      // console.log(baseURL + "?" + params.toString());
+
+      http.get(baseURL + "?" + params.toString(), (res) => {
+        let data = [];
+        res
+          .on("data", (chunk) => {
+            data.push(chunk);
+          })
+          .on("end", () => {
+            let weather = JSON.parse(Buffer.concat(data).toString());
+            console.log(weather);
+          })
+          .on("error", (err) => {
+            console.error(err);
+          });
+      });
+    }
+  )
+  .parse();
